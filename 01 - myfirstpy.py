97e92d082aad3160354cbb7142beafd8095786f1
janevis <64321654+janevis@users.noreply.github.com>
2020-04-30 19:36:50 +0300

    !

---
+++ 01 - myfirstpy.py
@@ -0,0 +1,17 @@
+print (( 23 + 8 ) % 24 )
+speed = 108
+time = 12
+dist = time * speed
+print (dist)
+speed1 = 108
+speed2 = 5
+time = 12
+dist = time * (speed1 + speed2)
+print (dist)
+phrase = "Hasta la vista"
+who = "baby"
+print (phrase ,",",  who )
+ans = 2 + 3
+expr = " 2 + 3 = "
+print (expr + str(ans))
+print (ans + 1)
